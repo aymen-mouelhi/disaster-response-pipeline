2baa1444705b4ab5c2404db5b479c28a46e9f6dc
Aymen Mouelhi <aymen.mouelhi@gmail.com>
2018-09-16 00:01:12 +0200

    implement the ETL pipeline

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,71 @@
-import sys
+'''
+ETL Pipeline that consists of three major parts:
+* Loading 2 datasets and merging them based on id
+* Cleaning the categories and removing duplicates
+* Storing the cleaned DataFrame into an sqlite database
+'''
 
+
+import sys
+import pandas as pd
+import sqlalchemy
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    # load messages dataset
+    messages = pd.read_csv(messages_filepath)
+    # load categories dataset
+    categories = pd.read_csv(categories_filepath)
+
+    # merge datasets
+    df = messages.merge(categories, how='inner', on=['id'])
+
+    return df
+
+
+def _get_processed_categories(df):
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(';', expand=True)
+
+    # select the first row of the categories dataframe
+    row = categories.iloc[0]
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything
+    # up to the second to last character of each string with slicing
+    category_colnames = row.apply(lambda x: x.split('-')[0])
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].apply(lambda x: x.split('-')[1])
+
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+
+    return categories
 
 
 def clean_data(df):
-    pass
+    # process categories
+    categories = _get_processed_categories(df)
+    # drop the original categories column from `df`
+    df = df.drop(['categories'], axis=1)
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+
+
+    # check number of duplicates
+    duplicated = df.duplicated()
+    duplicated_count = len(duplicated[duplicated == True])
+    # drop duplicates
+    df.drop_duplicates(inplace=True)
+
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    engine = sqlalchemy.create_engine('sqlite:///'+database_filename)
+    df.to_sql('categorized_messages', engine, index=False)
 
 
 def main():
@@ -24,12 +79,12 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
